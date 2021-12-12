c366fc9590f16fef538ad15812019e0a579f4932
Momo Ueda <umomo88@gmail.com>
2021-12-11 23:07:18 -0800

    base code for bar charts

--- consultation_graphs.py
+++ consultation_graphs.py
@@ -4,7 +4,29 @@
 #import packages
 import pandas as pd
 from matplotlib import pyplot as plt
-import seaborn as sns
 
 #read in exit survey data
 survey_raw = pd.read_csv("exit_responses.csv", encoding = 'utf-8')
+
+#create a list of colors to be used in the bar charts
+colors = ['#203864', '#4472c4', '#b4c7e7', '#a5a5a5', '#c00000']
+
+#function for creating the bar charts
+def bar_chart(df, colors):
+    """
+    This function creates bar charts from dataframes (df) as an input and loops through each column to create a bar graph (output) for each column. 
+    The function also takes in 'colors' as a parameter and should be a list of colors.
+    """
+    for column in df:
+        q = df[column].value_counts(normalize = True) 
+        q = pd.DataFrame(q).reset_index()
+        q.columns = ['answer', 'percent']
+    
+        plt.bar(q['answer'], q['percent'], color = colors)
+        plt.title(column)
+        plt.show()
+
+#take questions that are to be graphed as a bar chart for the consultation deck
+bars = survey_raw[['Question 1', 'Question 2a)', 'Question 2d)']]
+#run the function for bar charts
+bar_chart(bars, colors)
